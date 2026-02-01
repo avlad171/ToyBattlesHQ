63e610c224eab4e82bdd942afae2cfecd427cda9
avlad171 <blacklordakium@gmail.com>
2026-02-02 00:21:36 +0100

    Starting anticheat

---
+++ CastServer/include/Classes/AntiCheat.h
@@ -0,0 +1,50 @@
+//
+// Created by avlad171 on 22/01/2026.
+//
+
+#ifndef MICROVOLTS_EMULATOR_V2_ANTICHEAT_H
+#define MICROVOLTS_EMULATOR_V2_ANTICHEAT_H
+
+#include <vector>
+
+namespace Cast
+{
+    namespace Classes
+    {
+        constexpr size_t CIRCULAR_BUFFER_SIZE = 32;
+
+        struct PlayerPos
+        {
+            float x;
+            float y;
+            float z;
+
+            PlayerPos() : x(0.0f), y(0.0f), z(0.0f) {}
+            float distanceTo(PlayerPos &other) const;
+        };
+
+        struct PlayerState
+        {
+            uint64_t tick;
+            uint64_t hp;
+            //add more as needed
+
+            PlayerPos pos;
+            PlayerState () : tick(0), hp(0) {}
+        };
+
+        class AntiCheat
+        {
+            //we need a circular buffer to store player positions
+            PlayerState gameStates [16][CIRCULAR_BUFFER_SIZE];  //32 ticks should be enough
+            uint64_t tick;
+            AntiCheat(uint64_t tick);
+
+        public:
+            int movePlayer(uint64_t tick, unsigned int playerSlot, PlayerPos newPosition);
+            int attackMeleeNonhost(uint64_t tick, unsigned int attackerSlot, unsigned int targetSlot);
+
+        };
+    }
+}
+#endif //MICROVOLTS_EMULATOR_V2_ANTICHEAT_H
